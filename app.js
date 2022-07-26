f9aba12cc5a4871ae6b479b4202fa7243283cfde
Ronin Cunningham <ronincunningham1@gmail.com>
2022-07-25 23:36:00 -0400

    empty init

--- app.js
+++ app.js
@@ -22,19 +22,7 @@ var recipesRouter = require('./routes/recipes');
 
 var app = express();
 
-const whitelist = ["https://ronin-react-recipe-website.herokuapp.com/"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
